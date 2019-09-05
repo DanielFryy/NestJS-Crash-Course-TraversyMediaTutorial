d126ba1588621f6ad8a7bb204e6b544aec6a4508
DanielFryy <dgz1606@gmail.com>
2019-09-05 16:49:21 -0500

    Completion of tutorial

--- nest-rest-api/src/items/items.controller.ts
+++ nest-rest-api/src/items/items.controller.ts
@@ -1,36 +1,36 @@
-import {Body, Controller, Delete, Get, Param, Patch, Post, Put} from '@nestjs/common';
-import {CreateItemDto} from './dto/create-item.dto';
-import {ItemsService} from './items.service';
-import {Item} from './interfaces/item.interface';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
-  
+
   constructor(private readonly itemsService: ItemsService) { }
-  
+
   @Get()
-  findAll(): Item[] {
+  findAll(): Promise<Item[]> {
     return this.itemsService.findAll();
   }
-  
+
   @Get(':id')
-  findOne(@Param('id') id): string {
-    return `Item ${id}`;
+  findOne(@Param('id') id: string): Promise<Item> {
+    return this.itemsService.findOne(id);
   }
-  
+
   @Post()
-  create(@Body() createItemDto: CreateItemDto): string {
-    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(createItemDto);
   }
-  
+
   @Delete(':id')
-  delete(@Param('id') id): string {
-    return `Delete ${id}`;
+  delete(@Param('id') id: string): Promise<Item> {
+    return this.itemsService.delete(id);
   }
-  
-  @Patch(':id')
-  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
-    return `Update ${id} - Name: ${updateItemDto.name}`;
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id: string): Promise<Item> {
+    return this.itemsService.update(id, updateItemDto);
   }
-  
+
 }
--- nest-rest-api/src/items/items.service.ts
+++ nest-rest-api/src/items/items.service.ts
@@ -1,24 +1,32 @@
-import {Injectable} from '@nestjs/common';
-import {Item} from './interfaces/item.interface';
+import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class ItemsService {
-  private readonly items: Item[] = [
-    {
-      id: '15615615616',
-      name: 'Item One',
-      qty: 100,
-      description: 'This is item one',
-    },
-    {
-      id: '589596589496',
-      name: 'Item Two',
-      qty: 50,
-      description: 'This is item two',
-    },
-  ];
-  
-  findAll(): Item[] {
-    return this.items;
+
+  constructor(@InjectModel('Item') private readonly itemModel: Model<Item>) { }
+
+  async findAll(): Promise<Item[]> {
+    return await this.itemModel.find();
   }
+
+  async findOne(id: string): Promise<Item> {
+    return await this.itemModel.findOne({ _id: id });
+  }
+
+  async create(item: Item): Promise<Item> {
+    const newItem = new this.itemModel(item);
+    return newItem.save();
+  }
+
+  async delete(id: string): Promise<Item> {
+    return this.itemModel.findByIdAndRemove(id);
+  }
+
+  async update(id: string, item: Item): Promise<Item> {
+    return await this.itemModel.findByIdAndUpdate(id, item, { new: true });
+  }
+
 }
